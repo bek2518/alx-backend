3faec1a15242bfea6a5479335651a1f2829c0484
bek2518 <bereketb20@hotmail.com>
2023-05-23 16:13:43 +0300

    Detects if incoming request contains locale

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+'''
+Flask web application for i18n projects
+'''
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
+
+
+class Config(object):
+    '''
+    Config class that has a LANGUAGES class attribute that is used
+    to configure available languages in our app
+    '''
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    '''
+    Function that determines the best match with our supported languages
+    '''
+    if 'locale' in request.args:
+        requested_lang = request.args.get('locale')
+        if requested_lang in app.config['LANGUAGES']:
+            return requested_lang
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    '''
+    Index file that renders the index template
+    '''
+    return (render_template('4-index.html'))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
