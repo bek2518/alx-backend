c275ed34acf04a0399943ddaa6b5c4214b5882f6
bek2518 <bereketb20@hotmail.com>
2023-05-23 13:27:03 +0300

    Configure babe

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+'''
+Flask web application for i18n projects
+'''
+from flask import Flask, render_template, request
+from flask_babel import Babel, get_timezone, get_locale
+
+
+class Config(object):
+    '''
+    Config class that has a LANGUAGES class attribute that is used
+    to configure available languages in our app
+    '''
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+@app.route('/', strict_slashes=False)
+def index():
+    '''
+    Index file that renders the index template
+    '''
+    return (render_template('0-index.html'))
