852fd0925ce5dd6ab40378a3407fa0f0fb0b14f7
bek2518 <bereketb20@hotmail.com>
2023-06-19 11:46:50 +0300

    Stores hash using hset

---
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -0,0 +1,20 @@
+import { createClient } from 'redis';
+
+const client = createClient();
+
+client.on('error', (err) => console.log('Redis client not connected to the server: ', err));
+client.on('connect', () => console.log('Redis client connected to the server'));
+const values = {
+  Portland: 50,
+  Seattle: 80,
+  'New York': 20,
+  Bogota: 20,
+  Cali: 40,
+  Paris: 2,
+};
+
+for (const [key, value] of Object.entries(values)) {
+  client.hset('HolbertonSchools', key, value, (err, reply) => {
+    console.log('Reply: ', reply);
+  });
+}
