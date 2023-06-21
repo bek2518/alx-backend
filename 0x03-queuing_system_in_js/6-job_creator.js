d1f5a30b8d4873a2ea83fc3cb964ef161e696ed0
bek2518 <bereketb20@hotmail.com>
2023-06-21 14:14:06 +0300

    Queue process that listens to jobs

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -3,13 +3,13 @@ const kue = require('kue');
 const queue = kue.createQueue();
 
 const jobData = {
-  phoneNumber: 'string',
-  message: 'string',
+  phoneNumber: '4153518780',
+  message: 'This is the code to verify your account',
 };
 
 const job = queue.create('push_notification_code', jobData).save((err) => {
   if (!err) {
-    console.log('Notification job completed', job.id);
+    console.log('Notification job created:', job.id);
   } else {
     console.log('Notification job failed');
   }
